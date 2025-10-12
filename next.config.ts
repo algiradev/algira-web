fd0049babb23bb0b6bd75bdd3e401df84a1280e4
algiradev <algira.dev@gmail.com>
2025-10-12 11:57:04 -0400

    fix(http): fix url to api in production
    - edited url to api url in production

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
+    //local
+    // remotePatterns: [
+    //   {
+    //     protocol: "http",
+    //     hostname: "localhost",
+    //     port: "1337",
+    //     pathname: "/uploads/**",
+    //   },
+    // ],
+
+    //Producción
+    //https://algira-dev.onrender.com
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
+        protocol: "https",
+        hostname: "algira-dev.onrender.com",
+        port: "",
         pathname: "/uploads/**",
       },
     ],
